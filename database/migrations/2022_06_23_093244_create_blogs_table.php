760fda7ff7e08f8404d038905d6c967e7d5617cb
DevMamun <devmamun8@gmail.com>
2022-06-23 15:40:53 +0600

    Migration done

---
+++ database/migrations/2022_06_23_093244_create_blogs_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBlogsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('blogs', function (Blueprint $table) {
+            $table->id();
+            $table->string("title", 250)->nullable();
+            $table->text("description")->nullable();
+            $table->integer("category_id")->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('blogs');
+    }
+}
